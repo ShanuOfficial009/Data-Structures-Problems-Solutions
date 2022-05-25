94c02d1d8298d4153eafe487691338ebd7705472
Shanu kumar <93045419+ShanuOfficial009@users.noreply.github.com>
2022-05-25 15:26:17 +0530

    Update Max_Sub_Arr.java

--- Arrays_Questions/Max_Sub_Arr.java
+++ Arrays_Questions/Max_Sub_Arr.java
@@ -1,5 +1,12 @@
-package Arrays_Questions;
+//Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+//A subarray is a contiguous part of an array.
+/*Example 1:
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6. */
 
+
+package Arrays_Questions;
 public class Max_Sub_Arr {
 public static int solution(int [] arr){
     int csum = arr[0];
