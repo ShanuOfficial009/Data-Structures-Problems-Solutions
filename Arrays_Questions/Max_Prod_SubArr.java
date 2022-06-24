18a1e13130375c5dd754de5057f4c6ff20b11fa4
Shanu kumar <93045419+ShanuOfficial009@users.noreply.github.com>
2022-06-24 11:31:12 +0530

    Add files via upload

---
+++ Arrays_Questions/Max_Prod_SubArr.java
@@ -0,0 +1,16 @@
+package Arrays_Questions;
+
+public class Max_Prod_SubArr {
+    public int maxProduct(int[] nums) {
+        int max_overall = nums[0];
+        int max_ending_here = nums[0], min_ending_here = nums[0];
+
+        for(int i = 1; i < nums.length; ++i){
+            int tmp = max_ending_here;
+            max_ending_here = Math.max(nums[i], Math.max(nums[i]*max_ending_here, nums[i]*min_ending_here));
+            min_ending_here = Math.min(nums[i], Math.min(nums[i]*tmp, nums[i]*min_ending_here));
+            max_overall = Math.max(max_overall, max_ending_here);
+        }
+        return max_overall;
+    }
+}
